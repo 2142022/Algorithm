2f4652d92d276338009b1fbb89875dec63c90c8a
214 <nalee9801@gmail.com>
2023-10-16 12:57:34 +0900

    231016

--- 프로그래머스/Python/[Lv2] 60057 문자열 압축/문자열 압축.py
+++ 프로그래머스/Python/[Lv2] 60057 문자열 압축/문자열 압축.py
@@ -1,5 +1,3 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python3
-
 # 문자열 s를 unit 단위로 자르기
 def shorten(s, unit):
     # 압축 문자열의 길이
@@ -14,22 +12,13 @@ def shorten(s, unit):
             l = l + len(s) - i
             break
 
-        # 기준 문자열과 같으면 1, 아니면 0
-        flag = 1
-
-        # 기준 문자열과 같은지 체크
-        for j in range(unit):
-            if s[i + j] != s[i + j - unit]:
-                flag = 0
-                break
-
         # 기준 문자열과 같은 경우
-        if flag == 1:
-            # 처음으로 기준 문자열과 같은 경우나 기준 문자열의 개수가 처음으로 10개, 100개, 1000개가 되는 경우 +1
-            if cnt == 1 or cnt == 9 or cnt == 99 or cnt == 999:
-                l += 1
-
+        if s[i:i+unit] == s[i-unit:i]:
             cnt += 1
+            
+            # 처음으로 기준 문자열과 같은 경우나 기준 문자열의 개수가 처음으로 10개, 100개, 1000개가 되는 경우 +1
+            if cnt == 2 or cnt == 10 or cnt == 100 or cnt == 1000:
+                l += 1
 
         # 기존 문자열과 다른 경우
         else:
@@ -46,7 +35,7 @@ def solution(s):
     # 압축 문자열의 최소 길이 (문자열의 길이로 초기화)
     min_len = l
 
-    # 문자열을 2개 ~ (문자열의 길이의 반)씩 자르기
+    # 문자열을 2개 ~ 최대 (문자열의 길이의 반)까지 자르기
     for i in range(1, l // 2 + 1):
         min_len = min(min_len, shorten(s, i))
 
